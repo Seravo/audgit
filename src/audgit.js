13cd57061abe741ab4f294f009d39a074d348dbf
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2014-02-19 11:24:31 +0200

    Auto-initialize Audgit on each command. Generate package list on init. Make sure user has name and e-mail defined.

--- src/audgit.js
+++ src/audgit.js
@@ -41,6 +41,7 @@ var fs = require('fs');
 var domain = require('domain');
 var spawn = require('child_process').spawn;
 
+
 /*
  * Check that audgit was invoked correctly
  */
@@ -78,7 +79,7 @@ if (argv[0] == 'commit') {
   argv[1] = '--hard';
 }
 
-// TODO: major bug, reset deletes the hard link an replaces it with a non-linked file
+// TODO: BUG: reset deletes the hard link an replaces it with a non-linked file
 
 // TODO: check that /audgit exists or create it (error code ENOENT)
 // TODO: check that /audgit/.git exists or init it (error code ENOENT)
@@ -87,25 +88,42 @@ if (argv[0] == 'commit') {
 
 // console.dir(argv);
 
-// Stop reinitialization of Audgit
+
+// Make sure user has configured user.name
+var gitUserName = spawn('git', ['config', '--global', 'user.name'], {env: process.env});
+gitUserName.stdout.on('data', function (data) {
+  gitUserName.result = data;
+});
+gitUserName.on('close', function (data) {
+  if (gitUserName.result == undefined) {
+    showError('"git config --global user.name" must be defined to use Audgit!"', '', true);
+  }
+});
+
+// Make sure user has configured user.email
+var gitUserEmail = spawn('git', ['config', '--global', 'user.email'], {env: process.env});
+gitUserEmail.stdout.on('data', function (data) {
+  gitUserEmail.result = data;
+});
+gitUserEmail.on('close', function (data) {
+  if (gitUserEmail.result == undefined) {
+    showError('"git config --global user.email" must be defined to use Audgit!"', '', true);
+  }
+});
+
+
+
+// Make sure that the Audigt repository is created and initialized
+// for all commands, and output results it 'init' was the command
 if (argv[0] == 'init') {
-  try {
-    var audgitpath = fs.lstatSync('/audgit');
-  } catch (err) {
-    if (err.code != 'ENOENT') {
-      throw err;
-    } else {
-      fs.mkdirSync('/audgit');
-    }
-  }
-  if (audgitpath && audgitpath.isDirectory()) {
-    showError('Audgit already initialized', '', true);
-  } else if (audgitpath) {
-    showError('Remove current /audgit and try again.');
-  }
+  audgitInit({'explicit': true});
+} else {
+  audgitInit();
 }
 
-// Add to git repo via hard links
+
+// If command is 'add' prepare it by hard linking the file to a path under
+// the Audgit repository.
 if (argv[0] == 'add' && argv[1]) {
   // later use fs.realpathSync to make sure file has absolute path
   try {
@@ -153,8 +171,14 @@ if (argv[0] == 'add' && argv[1]) {
 }
 
 
+// Clean up any preceeding slashes to avoid Git from complaining
+// that the file is outside the git repository
+if (argv[1] && argv[1].substr(0, 1) == '/') {
+  argv[1] = argv[1].substr(1);
+}
+
+
 /*
- * Spawn git in child process
  * Run in separate domain to catch errors
  */
 
@@ -168,44 +192,138 @@ d.on('error', function(err) {
   }
 });
 
-// Clean up any preceeding slashes to avoid Git from complaining
-// that the file is outside the git repository
-if (argv[1] && argv[1].substr(0, 1) == '/') {
-  argv[1] = argv[1].substr(1);
+
+/*
+ * Main command is executed here by passing the command on to Git
+ * But execute it only command is not 'init' and previous init is OK
+ */
+if (argv[0] != 'init' && audgitInit()) {
+  d.run(function() {
+
+    // Prepare spawn to run Git in child process
+    var git = spawn(
+      'git',
+      argv,
+      {cwd: '/audgit', env: process.env}
+    );
+
+    git.stdout.on('data', onStdout);
+    git.stderr.on('data', onStderr);
+    git.on('close', onExit);
+
+  });
 }
 
-d.run(function() {
-
-  var git = spawn(
-    'git',
-    argv,
-    {cwd: '/audgit', env: process.env}
-  );
-
-  git.stdout.on('data', onStdout);
-  git.stderr.on('data', onStderr);
-  git.on('close', onExit);
-
-});
-
+/*
+ * Run additional commands after main command if needed
+ */
 if (['add', 'rm', 'reset'].indexOf(argv[0]) != -1) {
   // re-run to show status
   spawn('git', ['status'], {cwd: '/audgit', env: process.env, stdio: 'inherit'});
 }
 
-if (['init'].indexOf(argv[0]) != -1) {
+
+
+/*
+ * Generic init function that is run on all Audgit commands
+ */
+function audgitInit(args) {
+
+  if (args == undefined) {
+    var args = {};
+  }
+
+  // Make sure directory /audgit exists
+  try {
+    var audgitPath = fs.lstatSync('/audgit');
+  } catch (err) {
+    if (err.code != 'ENOENT') {
+      throw err;
+    } else {
+      fs.mkdirSync('/audgit');
+    }
+  }
+
+  if (audgitPath && audgitPath.isDirectory() === false) {
+    showError('Current /audgit is not a directory. Remove it and try again.');
+  }
+
+  // Make sure git repository in /audgit exists
+  try {
+    var audgitRepoPath = fs.lstatSync('/audgit/.git');
+  } catch (err) {
+    if (err.code != 'ENOENT') {
+      throw err;
+    } else {
+
+      // Audgit repository has not been created. Creating it now..
+
+      // Prepare spawn to run Git in child process
+      var gitInit = spawn(
+        'git',
+        ['init'],
+        {cwd: '/audgit', env: process.env}
+      );
+
+      gitInit.stdout.on('data', onStdout);
+      gitInit.stderr.on('data', onStderr);
+      gitInit.on('close', function(code) {
+        if (code == 0) {
+          // 0 is OK
+          audgitInitPopulate();
+        } else {
+          showError('Child process exited with code', code);
+        }
+      });
+
+    }
+  }
+
+  if (audgitPath && audgitPath.isDirectory()) {
+    if (args.explicit) {
+      showError('Audgit repository was already created', '', true);
+    }
+  } else if (audgitPath) {
+    showError('Current /audgit/.git is not a directory. Remove it and try again.');
+  }
+
+  return true;
+}
+
+/*
+ * Populate new Audgit repository with basic data
+ */
+function audgitInitPopulate() {
   // rename master branch to hostname
   var os = require('os');
   spawn('git', ['checkout', '-b', os.hostname()], {cwd: '/audgit', env: process.env, stdio: 'inherit'});
+
   // TODO: do an initial commit with basic OS info (platform, release) and installed packages list
+  var dpkgGetSelections = spawn('dpkg', ['--get-selections'], {cwd: '/audgit', env: process.env});
+
+  dpkgGetSelections.stdout.on('data', function (data) {
+    fs.appendFile('/audgit/dpkg-selections', data, function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+
+  dpkgGetSelections.on('close', function (data) {
+    var gitAdd = spawn('git', ['add', '--all', '/audgit'], {cwd: '/audgit', env: process.env, stdio: 'inherit'});
+    gitAdd.on('close', function (data) {
+      spawn('git', ['commit', '-am', '"First commit with package list"'], {cwd: '/audgit', env: process.env, stdio: 'inherit'});
+    });
+  });
 
   // Copy git hook
   copyFile('scripts/post-commit', '/audgit/.git/hooks/post-commit', function(err) {
-    if (err) { throw err; }
+    if (err) {
+      throw err;
+    } else {
+      fs.chmodSync('/audgit/.git/hooks/post-commit', '755');
+    }
   });
-  // TODO: line below yielded error while installation in the wild,
-  //       investigate possible race condition and why post-commit file was empty
-  fs.chmodSync('/audgit/.git/hooks/post-commit', '755');
 
   // Copy MOTD script if system supports it
   // TODO: try just making symbolic links instead of copying
@@ -245,7 +363,7 @@ function onExit(code) {
   if (code == 0) {
     // 0 is OK
   } else if (code == 128) {
-    showError('The Audgit repository does not exists. Run "audgit init".', '', true);
+    showError('The Audgit repository does not exist. Run "audgit init".', '', true);
   } else {
     showError('Child process exited with code', code);
   }
